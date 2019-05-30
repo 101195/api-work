0ca6d9f5ecf780e3531cf01eabc495cbac942279
101195 <31431495+101195@users.noreply.github.com>
2019-05-30 12:56:24 +0300

    Add files via upload

--- api-work/rats/screencaster.php
+++ api-work/rats/screencaster.php
@@ -4,13 +4,9 @@
             echo "Multiplication of the two numbers is : $ans";
          }
 
-         if ($ans=20 ){
-         	echo "true twenty";
-         }
-         elseif ($num2==string) {
-         	echo "raten is not a number";
-         	# code...
-         }
+
+       
+      
          
          addFunction(3, 1);
 
@@ -21,4 +17,23 @@
          addFunction(5,4.5);
 
          addFunction(3.4,5);
+
+
+
+           if ($num1 == 0 || $num2 == 0) {
+        return 0;
+    }
+
+    if ($num2 < 0) {
+        return add(-$num1, -$num2);
+    }
+
+    // Since we use recursion we need a base case.
+    if ($num2 == 1) {
+        return i;
+    }
+
+    // Define our problem in terms of the same problem, but of smaller size. 
+    return $num1 + add($num1, $num2 - 1);
+
       ?>
