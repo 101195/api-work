c75ecb03f1bcd1202575890216251c20835e622c
101195 <31431495+101195@users.noreply.github.com>
2019-05-30 12:32:14 +0300

    Add files via upload

---
+++ api-work/rats/screencaster.php
@@ -0,0 +1,24 @@
+ <?php
+         function addFunction($num1, $num2) {
+            $ans = $num1 * $num2;
+            echo "Multiplication of the two numbers is : $ans";
+         }
+
+         if ($ans=20 ){
+         	echo "true twenty";
+         }
+         elseif ($num2==string) {
+         	echo "raten is not a number";
+         	# code...
+         }
+         
+         addFunction(3, 1);
+
+         addFunction(4,5);
+
+         addFunction(1,raten);
+
+         addFunction(5,4.5);
+
+         addFunction(3.4,5);
+      ?>
